78163a9cfbcbf1f12bdae91793776db11be1239e
赵晓放 <zhaoxiaofang@dishifenqi.com>
2017-09-12 10:23:12 +0800

    22

--- testGit/src/com/testGit/controller/TestController.java
+++ testGit/src/com/testGit/controller/TestController.java
@@ -1,5 +1,14 @@
 package com.testGit.controller;
 
+/**
+ * testModel类
+ * 
+ * TestController
+ * 创建人：zhaoxf
+ * 修改人：zhaoxf
+ * 2017年9月12日 上午10:21:55
+ * @version 1.0.0
+ */
 public class TestController {
-
+	
 }
